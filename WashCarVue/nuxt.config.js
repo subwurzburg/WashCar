96d6ba99741cc50426d22d3fd24502a1df0727c9
subwurzburg24 <you@example.com>
2023-04-14 12:17:33 +0800

    gd

--- WashCarVue/nuxt.config.js
+++ WashCarVue/nuxt.config.js
@@ -30,7 +30,9 @@ export default {
     '@nuxtjs/tailwindcss'
     // https://go.nuxtjs.dev/tailwindcss,
   ],
-
+  server: {
+    port: 6000
+  },
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
